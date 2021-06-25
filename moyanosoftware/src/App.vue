96e17bc9cad86ce6a31cd03bb4185400fb3f1d32
Nicolas Escobar <u20161148263@usco.edu.co>
2021-06-25 17:39:22 -0500

    start

--- moyanosoftware/src/App.vue
+++ moyanosoftware/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Inicio</router-link> /
+      <router-link to="/SIINET">SIINET</router-link> /
+      <router-link to="/nosotros">Nosotros</router-link> /
+      <router-link to="/ayuda">Ayuda</router-link> /
+      <router-link to="/contacto">Contacto</router-link>
     </div>
     <router-view/>
   </div>
