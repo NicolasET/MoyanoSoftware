f8ed2f46bde3e3ce42371aef8c5151bb7b830c13
Nicolas Escobar <u20161148263@usco.edu.co>
2021-06-28 15:47:18 -0500

    blue header

--- moyanosoftware/src/App.vue
+++ moyanosoftware/src/App.vue
@@ -1,24 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="navbar">
+      <img src="./assets/moyanologo.png" width="187" height="90" class="logo">
       <router-link to="/">Inicio</router-link> /
       <router-link to="/SIINET">SIINET</router-link> /
       <router-link to="/nosotros">Nosotros</router-link> /
       <router-link to="/ayuda">Ayuda</router-link> /
-      <router-link to="/contacto">Contacto</router-link> /
+      <router-link to="/contacto">Contacto</router-link>
       <router-link to="/cotizar">COTIZAR</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+
 <style>
-#app {
+.logo {
+  position: absolute;
+  left: 319px;
+}
+#nav {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: right;
-  color: #353b8d;
+  color: #754448;
+}
+#nav a{
+  font-weight: bold;
+  color: #ffffff;
+}
+.navbar{
+    width: 880px;
+    height: 90px;
+    transform: skew(160deg);
+    background: #0A8CBF;
+  
+}
+
+/*#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: right;
+  color: #754448;
 }
 
 #nav {
@@ -32,5 +57,5 @@
 
 #nav a.router-link-exact-active {
   color: #9867d1;
-}
+}*/
 </style>
--- moyanosoftware/src/views/Home.vue
+++ moyanosoftware/src/views/Home.vue
@@ -4,16 +4,6 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>titulo en página principal </h1>
   </div>
+
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
