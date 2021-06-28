911f3176fdbf41ad26d85445e6824cdce4e01acc
jorgeduque <ivanduque-2003@hotmail.com>
2021-06-28 15:45:50 -0500

    texto SIINET

--- moyanosoftware/src/views/SIINET.vue
+++ moyanosoftware/src/views/SIINET.vue
@@ -1,17 +1,98 @@
+@import url(https://fonts.google.com/specimen/Titillium+Web);
 <template>
-  <div id= "siineteditable" class="siinet">
-    <h1>SIINET</h1>
+  <div  id= "cuerpoTextoSIINET" class="siinet">
+    <h1 id="titulos">¿QUÉ ES SIINET?</h1>
+    <p id="subtitulos">Sistema integrado basados en diseños modulares.</p>
+    <br>
+    <p id="parrafos">SIINET es un aplicativo robusto que Permite llevar el control total e integrado 
+      de todas las áreas de la entidad, es seguro e integral, controla toda la administración 
+      financiera en entidades del sector solidario de Aportes, Ahorros, Créditos, Multiactivas 
+      y con actividad Financiera.</p>
+
+    <p id="parrafos">El Software ERP: SIINET, cuenta con más de 60 clientes instalados operando en línea.</p>
+    <p id="parrafos">Muy Amigable: Interfaz grafica, la cual permite la gestión operativa, administrativa y Financiera.</p>
+    <p id="parrafos">SIINET® Cumple con las normas de control y vigilancia y genera informes para reportar a la SUPERSOLIDARIA,
+       DIAN, FOGACOOP y centrales de riesgo como son CIFIN Y DATACRÉDITO y NORMAS NIIF.</p>
+    <p id="subtitulos">CARACTERÍSTICAS PRINCIPALES:</p>
+    <br>
+    <p id="parrafos">Parametrizable</p>
+    <p id="parrafos">Procesos Automáticos</p>
+    <p id="parrafos">Adopción a las NICs y NIIFs</p>
+    <p id="parrafos">Modular</p>
+    <p id="parrafos">Detección de usuarios a través de características biométricas (huella).</p>
+    <p id="parrafos">Control de acceso por usuario.</p>
+    <p id="parrafos">Exportación de archivos a otras herramientas (Excel, Word) directamente.</p>
+    <p id="parrafos">Generación automática de formatos (pagarés, cheques, libranzas, facturas, solicitudes, etc).</p>
+    <p id="parrafos">Envío automático de extractos por correo electrónico.</p>
+    <br>
+    <p id="subtitulos">PROCESOS AUTOMÁTICOS:</p>
+    <br>
+    <p id="parrafos">• Liquidación del GMF.</p>
+    <p id="parrafos">• Control Tasa de Usura.</p>
+    <p id="parrafos">• Calificación y Reclasificación de Cartera.</p>
+    <p id="parrafos">• Ley de Arrastre de Créditos.</p>
+    <p id="parrafos">• Pre liquidación de Créditos.</p>
+    <p id="parrafos">• Provisión.</p>
+    <p id="parrafos">• Liquidación y causación de Intereses de cartera.</p>
+    <p id="parrafos">• Depreciación.</p>
+    <p id="parrafos">• Ahorros y depósitos.</p>
+    <p id="parrafos">• Generación y aplicación de descuentos de cartera por nómina.</p>
+    <p id="parrafos">• Pre-retiro automático de clientes.</p>
+    <p id="parrafos">• Reestructuración de Créditos.</p>
+    <p id="parrafos">• Conciliaciones bancarias.</p>
+    <p id="parrafos">• Manejo de Vacaciones.</p>
+    <p id="parrafos">• Distribución de excedentes.</p>
+    <p id="parrafos">• Giros de Créditos.</p>
+    <p id="parrafos">• Control de cupos.</p>
+    <p id="parrafos">• Reclasificación de cuentas de Ahorro Inactivas.</p>
+    <p id="parrafos">• Retanqueo de Créditos.</p>
+    <p id="parrafos">• El tablero del Balanced Scorecard proporciona indicadores contables, financieros y administrativos.</p>
+    <br>
+    <br>
+    <p id="parrafos"><span  style="color:#035AA6">SIINET </span><span  style="color:#000000">Cuenta con el mejor servicio de Soporte y Actualización. Veinte (20) años de experiencia.</span></p>
+    <br>
+    <p id="subtitulos">Disponible para:</p>
+    <p id="parrafos">Windows 8 y Windows 10.</p>
+    <p id="parrafos">Base de datos: SYBASE, Interactive SQL y Postgresql.</p>
+    <p id="parrafos">8 GB RAM.</p>
+
+     
   </div>
 </template>
 
 <style>
-#siineteditable {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+#titulos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 77px;
+  font-weight: bold;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #0D1C38;
 }
-
+#subtitulos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #035AA6;
+}
+#parrafos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #000000;
+}
+#cuerpoTextoSIINET {
+  margin-right: 231px;
+  margin-left: 231px;
+  margin-top: 163px;
+  margin-bottom: 538px
+}
 
 </style>
