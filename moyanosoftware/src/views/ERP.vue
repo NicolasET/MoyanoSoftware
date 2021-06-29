68ba0f2f7aae7a7a816777e4fa03fe177da411fe
jorgeduque <ivanduque-2003@hotmail.com>
2021-06-29 12:24:30 -0500

    Texto ERP

---
+++ moyanosoftware/src/views/ERP.vue
@@ -0,0 +1,103 @@
+@import url(https://fonts.google.com/specimen/Titillium+Web);
+<template>
+  <div  id= "cuerpoTextoERP" class="ERP">
+    <h1 id="titulos">Sistema ERP</h1>
+    <p id="parrafos"><span  style="color:#035AA6">La Planificación de Recursos Empresariales (Enterprise Resource Planning – ERP) </span><span  style="color:#000000">, es un sistema de información diseñado con el objetivo de incrementar la eficiencia de los recursos de los que dispone una empresa. Además, tiene la capacidad de adaptarse a las necesidades particulares de cada organización.
+</span></p>
+    <br>
+    <p id="parrafos">Los sistemas ERP se encargan de establecer una línea de comunicación eficaz a través de los procesos clave de una empresa, tales como: fabricación, compra, inventariado, control financiero, ventas, atención al cliente, entre otros.</p>
+
+    <p id="parrafos">Con un sistema ERP integrado en su empresa, las barreras de información entre los diferentes departamentos se hacen más tenues, logrando acciones coordinadas y consecuentes con los objetivos organizacionales.</p>
+
+    <p id="subtitulos">Beneficios de un ERP en tu empresa:</p>
+    <br>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 1: </span><span  style="color:#000000">Ahorro a largo plazo.</span></p>
+    <p id="parrafos">Realmente uno de los mayores beneficios es, que con un sistema ERP ahorramos dinero. De su definición mas básica entendemos que una solución de este tipo nos gestiona nuestro negocio, nuestros procesos, nuestros bienes materiales o inmateriales, nuestros recursos humanos o nuestros clientes. Es una gestión mas eficiente lo que normalmente lleva a un ahorro. Es así de simple. Eso si, es un ahorro muy a largo plazo, no hay que olvidarlo.</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 2: </span><span  style="color:#000000">Toma de decisiones.</span></p>
+    <p id="parrafos">Se dice de que un software de este tipo es una decisión estratégica y básicamente tienen toda la razón del mundo. Nos facilita tomar una decisión por que tenemos los datos de toda la empresa en una pantalla. Por ejemplo un ERP puede traer un cuadro de mandos, como si de un coche se tratara con la información de como marcha la empresa en este preciso momento. Al tener los datos siempre disponibles de todos los departamentos y una planificación bien definida dentro del sistema es lógico que podamos tomas las decisiones importantes con mas facilidad.
+</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 3: </span><span  style="color:#000000">Calidad o relación con el cliente.</span></p>
+    <p id="parrafos">Una correcta implantación de un ERP nos permite responder ante el cliente en un tiempo muchísimo mas reducido. Por ejemplo imagínense que vendemos queso, pues si en algún momento tenemos una reclamación de que un lote ha producido cagalera, podremos encontrar en muy poco tiempo el origen de la leche, al proveedor, el almacén donde se había guardado, las fechas de caducidad... etc Básicamente nos ofrece una trazabilidad mucho mas rapida. Por no hablar de la posibilidad de incorporar el modulo de CRM (Customer Relationship Management) a nuestra aplicación lo que nos hará aun mas eficientes a la hora de relación con los clientes.</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 4: </span><span  style="color:#000000">Seguridad.</span></p>
+    <p id="parrafos">La información crucial de la empresa estará debidamente protegida en dos sentidos. En el sentido del robo de información o acceso desautorizado a ella así como la seguridad de los datos. Todos las soluciones que hemos visto incorporan distintos niveles de acceso o autorización. Por otro lado la información estará centralizada y con copias de seguridad programadas automáticamente para prevenir cualquier fallo. En el caso de soluciones en la nube la seguridad de los datos es incluso mayor al estar replicados en distintos lugares.
+</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 5: </span><span  style="color:#000000">Productividad de los empleados.</span></p>
+    <p id="parrafos">Quizás es el beneficio mas evidente y lógico de implantación de un ERP, ademas se deriva de la propia definición como en el punto 1. Un sistema ERP optimiza la gestión de procesos por consiguiente aumenta la productividad de los empleados. Se eliminan los trabajos duplicados, se elimina la información redundante o se automatizan los procesos. Por ejemplo imagínense que implantamos la validación de un campo que supone el peso total de la mercancía para un camión. Un error aquí puede ser crucial en el negocio de una empresa, el vehículo simplemente pueden parar en la frontera. Pero con una simple validación en el formulario evitamos la "tragedia".</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 6: </span><span  style="color:#000000">Estandariza la organización.</span></p>
+    <p id="parrafos">Cuando toda la plantilla de distintos departamentos trabaja con el mismo programa, esta aplicación les obliga a ser mas "estándar" o mas ordenados. Les ayuda a reflexionar sobre los procesos o la manera de trabajar de cada departamento lo que lleva a la reflexión sobre el proceso y la consiguiente mejora. Básicamente ayuda a definir las buenas practicas dentro de la plantilla de la empresa.</p>
+    <p id="parrafos"><span  style="color:#035AA6">Beneficio 7: </span><span  style="color:#000000">Impulsa a crecer ordenadamente.</span></p>
+    <p id="parrafos">Normalmente todas las empresas quieren crecer a corto o largo plazo. Un sistema ERP posibilita un crecimiento mucho mas ordenado y menos doloroso. De hecho un crecimiento puede llegar a ser muy traumático para una empresa ya que genera tensiones en toda la estructura. Al tener la visibilidad de la imagen de una empresa en un preciso momento en una pantalla, podemos determinar mas fácilmente donde esta el punto flojo en la estructura de la empresa que imposibilita el crecimiento o que hay que reforzar. Aquí realmente nos apoyamos en el punto numero 2, en la mejora de la toma de decisiones en una empresa.</p>
+    <br>
+    <p id="subtitulos">Ventajas de implantar un ERP en tu empresa:</p>
+    <br>
+    <p id="parrafos"><span  style="color:#035AA6">1. </span><span  style="color:#000000">Mejora el proceso de toma de decisiones. </span></p>
+    <p id="parrafos">Tal y como adelantábamos al principio del artículo, los sistemas ERP permiten crear una base de datos compartida con más información y de mayor calidad que los responsables podrán visualizar en su pantalla en tiempo real agilizando y mejorando el proceso de toma de decisiones.
+</p>
+    <p id="parrafos"><span  style="color:#035AA6">2. </span><span  style="color:#000000">Planificación realista de los escenarios futuros. </span></p>
+    <p id="parrafos">Al disponer de información de mayor calidad es posible realizar estimaciones y previsiones realistas anticipándonos a los escenarios futuros.</p>
+    <p id="parrafos"><span  style="color:#035AA6">3. </span><span  style="color:#000000">Las mínimas duplicidades. </span></p>
+    <p id="parrafos">En las empresas que todavía no cuentan con una solución ERP es fácil comprobar que sus departamentos actúan con cierta independencia unos de otros, por lo que acaban generando registros e informes por duplicado. La integración que supone el ERP permitirá disminuir drásticamente estas duplicidades.</p>
+    <p id="parrafos"><span  style="color:#035AA6">4. </span><span  style="color:#000000">Los sistemas ERP son modulares. </span></p>
+    <p id="parrafos">Los ERP están formados por módulos. De esta manera podrás implantar los módulos que tu empresa necesite en cada momento. Si en el futuro tu empresa evoluciona y crece tu ERP lo hará con ella si incorporas nuevas funcionalidades.</p>
+    <p id="parrafos"><span  style="color:#035AA6">5. </span><span  style="color:#000000">Total adaptación a las necesidades de tu empresa. </span></p>
+    <p id="parrafos">Todas las empresas son diferentes. Por ello el proveedor de tu sistema ERP deberá realizar un análisis previo sobre las necesidades de tu organización para garantizar que la aplicación se adapta perfectamente a ella. La solución ERP debe adaptarse a tu negocio, no al revés.</p>
+    <p id="parrafos"><span  style="color:#035AA6">6. </span><span  style="color:#000000">Mayor control y trazabilidad. </span></p>
+    <p id="parrafos">Los sistemas ERP facilitan el seguimiento del producto a lo largo de la organización, desde que entra la materia prima hasta la entrega del producto acabado a los clientes.</p>
+    <p id="parrafos"><span  style="color:#035AA6">7. </span><span  style="color:#000000">Integración con el resto de los miembros de la cadena de valor. </span></p>
+    <p id="parrafos">El nuevo concepto ERP II supera los límites de la empresa añadiendo a otros integrantes de la cadena de valor: clientes y proveedores. Así el proveedor pasa a ser un socio estratégico del que podemos extraer una ventaja competitiva al participar activamente en el diseño y desarrollo de nuevos productos aportando su experiencia.</p>
+    <p id="parrafos">Respecto a los clientes, los ERP permiten contar con más información y de mayor calidad que permitirá ajustar las órdenes de producción y de compra a sus necesidades. Por otro lado dispondremos de una base de datos sobre nuestros clientes segmentada que nos permitirá realizar campañas de marketing orientadas a segmentos específicos.</p>
+    <p id="parrafos"><span  style="color:#035AA6">8. </span><span  style="color:#000000">Mejor comunicación interna. </span></p>
+    <p id="parrafos">El ERP suprime las barreras a la información entre los distintos departamentos integrándolos y mejorando la comunicación interna.</p>
+    <p id="parrafos"><span  style="color:#035AA6">9. </span><span  style="color:#000000">Automatización de tareas. </span></p>
+    <p id="parrafos">Los sistemas ERP permiten automatizar tareas pesadas y repetitivas que anteriormente se realizaban a mano (generación de albaranes, facturas etcétera).</p>
+    <p id="parrafos">De ese modo nuestros operarios podrán dedicarse a tareas más productivas que realmente son las que nos aportan valor. Pero es más, al reducirse el componente humano, caerá el número de errores (aunque no se eliminará del todo dado que seguirán siendo necesarias ciertas acciones manuales como la introducción de datos).</p>
+    <p id="parrafos"><span  style="color:#035AA6">10. </span><span  style="color:#000000">Menos costes, más competitividad. </span></p>
+    <p id="parrafos">La mayor eficiencia conseguida, la disminución en el número de errores y la reducción de tareas duplicadas permiten disminuir los costes empresariales. La reducción de costes nos va a permitir:</p>
+    <p id="parrafos">*Aumentar nuestro margen de beneficios al caer el coste unitario.<br>*Reducir el precio de cada producto al disminuir su coste.<br>*O ambas.</p>
+    <p id="parrafos"><span  style="color:#035AA6">11. </span><span  style="color:#000000">Aumento del rendimiento y del retorno de la inversión en el largo plazo. </span></p>
+    <p id="parrafos">Gracias a todas las ventajas anteriores se podrá conseguir un incremento de la rentabilidad y de los beneficios. En otras palabras, el aumento del rendimiento conseguido con la implantación del ERP compensa, en el largo plazo, la inversión inicial realizada.</p>
+    <br>
+    <p id="parrafos"><span  style="color:#000000">En </span><span  style="color:#035AA6">conclusión </span><span  style="color:#000000">si bien es cierto que implantar un ERP es un proceso complejo, no necesariamente debe ser doloroso si se gestiona adecuadamente y se dispone del equipo adecuado para líder el cambio. La recompensa la encontraremos en las enormes ventajas que reportará a la empresa que incorpore esta solución… ¿Estás preparado para sumarte al cambio? </span></p>
+
+   
+  </div>
+</template>
+
+<style>
+#titulos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 77px;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #0D1C38;
+}
+#subtitulos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #035AA6;
+}
+#parrafos {
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #000000;
+}
+#cuerpoTextoERP {
+  margin-right: 231px;
+  margin-left: 231px;
+  margin-top: 163px;
+  margin-bottom: 538px;
+  line-height:30px;
+  
+}
+
+</style>
