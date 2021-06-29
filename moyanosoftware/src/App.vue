2a1dbb1ca5f5af8ac8e1e9e0501aff00fb13324a
Nicolas Escobar <u20161148263@usco.edu.co>
2021-06-29 12:48:14 -0500

    Navegacion

--- moyanosoftware/src/App.vue
+++ moyanosoftware/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav" class="navbar">
+    <div id="logo">
       <img src="./assets/moyanologo.png" width="187" height="90" class="logo">
+    </div>
+
+    <div id="nav3">
       <router-link to="/">Inicio</router-link> /
       <router-link to="/SIINET">SIINET</router-link> /
       <router-link to="/nosotros">Nosotros</router-link> /
@@ -16,27 +19,27 @@
 
 
 <style>
-.logo {
+#logo {
   position: absolute;
   left: 319px;
 }
-#nav {
+#nav3 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: right;
   color: #754448;
+  width: 880px;
+  height: 90px;
+  margin-right: 0px;
+  margin-left: 1030px;
+  background: #0A8CBF;
 }
-#nav a{
-  font-weight: bold;
+#nav3 a{
   color: #ffffff;
 }
-.navbar{
-    width: 880px;
-    height: 90px;
-    transform: skew(160deg);
-    background: #0A8CBF;
-  
+#nav3 a.router-link-exact-active{
+  color: #000000;
 }
 
 /*#app {
